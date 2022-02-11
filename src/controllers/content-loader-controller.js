9105ea9e9fa1e1f9c66ad3e9d916728c80d359a3
Marcio Anderson Cezar de Araujo <contato@overclick.com.br>
2022-02-11 14:14:48 -0300

    Update content-loader-controller.js

--- src/controllers/content-loader-controller.js
+++ src/controllers/content-loader-controller.js
@@ -3,12 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static values = { url: String, refreshInterval: Number }
 
-    startRefershing() {
-        setInterval(() => {
-            this.load()
-        }, this.refreshIntervalValue);
-    }
-
     connect() {
         this.load()
 
@@ -17,9 +11,25 @@ export default class extends Controller {
         }
     }
 
+    disconnect() {
+        this.stopRefreshing()
+    }
+
     load() {
         fetch(this.urlValue)
             .then(response => response.text())
             .then(html => this.element.innerHTML = html)
+    }
+
+    startRefershing() {
+        this.refreshTimer = setInterval(() => {
+            this.load()
+        }, this.refreshIntervalValue);
+    }
+
+    stopRefreshing() {
+        if (this.refreshTimer) {
+            clearInterval(this.refreshTimer)
+        }
     }
 }
